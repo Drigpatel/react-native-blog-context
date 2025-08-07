cd25dd0f3223ea17b88b3321a7d8c61059b94bd4
Drigpatel <pdirg066@gmail.com>
2025-08-07 16:25:21 -0400

    Update AnotherScreen

--- src/screens/AnotherScreen.js
+++ src/screens/AnotherScreen.js
@@ -1,12 +1,1 @@
-import React from 'react';
-import { View, Text } from 'react-native';
 
-const AnotherScreen = () => {
-  return (
-    <View>
-      <Text>Another Screen</Text>
-    </View>
-  );
-};
-
-export default AnotherScreen;
